e5ec42c49d9539e5f964924417b410c1b418ca38
Laurencio Paez <73235321+laurenciopaez@users.noreply.github.com>
2025-02-06 10:36:13 -0300

    Add files via upload

---
+++ curso2-py avanzado/ej2.py
@@ -0,0 +1,15 @@
+"""The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal."""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    scores = student_marks[query_name]
+    summ = sum(scores)
+    average = summ/ len(scores)
+    print(f"{average:.2f}")
